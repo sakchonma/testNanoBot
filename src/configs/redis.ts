f7ab394ac0eaa4eefe9d0fe4b949aacbf3cf8a27
sakchonma <sakchonma@gmail.com>
2025-09-13 23:32:19 +0700

    update service

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -4,16 +4,12 @@ let redisClient: Redis | null = null;
 
 export const getRedisClient = (): Redis => {
     if (!redisClient) {
-        const server_env = process.env.SERVER || '0';
         const redis_env = process.env.REDIS_URL || '';
-        const vercel_redis_env = process.env.VERCEL_REDIS_URL || '';
-
-        const connect_redis = server_env === '1' ? vercel_redis_env : redis_env;
-        if (!connect_redis) {
+        if (!redis_env) {
             throw new Error('Redis URL is not defined');
         }
-
-        redisClient = new Redis(connect_redis);
+        console.log(redis_env)
+        redisClient = new Redis(redis_env);
         redisClient.on('connect', () => console.log('Redis Ready!'));
         redisClient.on('error', (err) => console.error('Redis error:', err));
     }
